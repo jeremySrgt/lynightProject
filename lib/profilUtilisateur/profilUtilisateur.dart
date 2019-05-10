101584bcf02d03124497598bc28ee4b6b0ba4669
sokdavid <david.sok@edu.esiee.fr>
2019-05-10 13:36:04 +0200

    page de profil utilisateur

--- lib/profilUtilisateur/profilUtilisateur.dart
+++ lib/profilUtilisateur/profilUtilisateur.dart
@@ -3,13 +3,12 @@ import 'package:flutter/material.dart';
 class UserProfil extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    Widget personSection = Container( // description de la personne
+    Widget personSection = Container(
+      // description de la personne
       padding: const EdgeInsets.all(32),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-
           Container(
             padding: const EdgeInsets.only(bottom: 8),
             child: Text(
@@ -23,14 +22,15 @@ class UserProfil extends StatelessWidget {
             'Pseudo',
             style: TextStyle(
               fontWeight: FontWeight.bold,
-
             ),
           ),
           Text(
             'Date de naissance',
-            style: TextStyle(
-
-            ),
+            style: TextStyle(),
+          ),
+          Text(
+            'Localisation',
+            style: TextStyle(),
           ),
         ],
       ),
@@ -42,15 +42,12 @@ class UserProfil extends StatelessWidget {
         children: [
           Icon(
             Icons.music_note,
-            color: Colors.orange,
+            color: Colors.deepOrange,
           ),
           Text(
             'Style de musique :',
-            style: TextStyle(
-
-            ),
+            style: TextStyle(),
           )
-
         ],
       ),
     );
@@ -61,33 +58,48 @@ class UserProfil extends StatelessWidget {
         children: [
           Icon(
             Icons.mail,
-            color: Colors.orange,
+            color: Colors.deepOrange,
           ),
           Text(
             'Adresse Mail :',
-            style: TextStyle(
-
-            ),
+            style: TextStyle(),
           )
+        ],
+      ),
+    );
 
+    Widget phoneSection = Container(
+      padding: const EdgeInsets.all(5),
+      child: Row(
+        children: [
+          Icon(
+            Icons.phone,
+            color: Colors.deepOrange,
+          ),
+          Text(
+            'Portable: :',
+            style: TextStyle(),
+          )
         ],
       ),
     );
 
     return Column(
-            children: [
-              Image.asset(
-                'assets/boite.jpg',
-                width: 220,
-                height: 308,
-                fit: BoxFit.cover,
-              ),
-              personSection,
-              styleSection,
-              infoSection
-            ],
-          );
+      children: [
+        CircleAvatar(
+           backgroundImage: ExactAssetImage('assets/test.jpg'),
+            minRadius: 90,
+            maxRadius: 150,),
+        styleSection,
+        infoSection,
+        phoneSection,
+        const RaisedButton(
+          onPressed: null,
+          child: Text('Modifier mon profil', style: TextStyle(fontSize: 20)),
+        ),
+      ],
+    );
   }
-
 }
+
 
