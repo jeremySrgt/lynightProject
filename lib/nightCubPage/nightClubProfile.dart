79ea8d8cfd99e1d8349fc75e2655454b0004b44a
BastinMax <maxed.bastin@gmail.com>
2019-05-13 21:00:24 +0200

    Update nightProfile section with information and musique but bad display

--- lib/nightCubPage/nightClubProfile.dart
+++ lib/nightCubPage/nightClubProfile.dart
@@ -2,49 +2,37 @@ import 'package:flutter/material.dart';
 
 class NightClubProfile extends StatelessWidget {
   @override
-    Widget build(BuildContext context) {
-
-
-    Widget bigRoundedBoxes = Container(
-      padding: const EdgeInsets.all(10),
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: Column(
-              crossAxisAlignment:
-                  CrossAxisAlignment.start, //met dans le bon axe
-              children: [
-                Container(
-                  padding: EdgeInsets.only(top: 16),
-                  width: 500, // Dimensions du carré rouge
-                  height: 400,
-                  decoration: BoxDecoration(
-                    color: Colors.redAccent,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(32),
-                        topRight: Radius.circular(32)),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-
-
-
-    Widget upperSection = Container(
-      padding: const EdgeInsets.all(20), //offset de 50
+  Widget build(BuildContext context) {
+    Widget infoSection = Expanded(
       child: Row(
         children: [
-          Expanded(
+          Container(
+            margin: EdgeInsets.only(left: 50),
+           // width: 275,
+            //height: 150,
+            /*decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              //color: Colors.blueGrey,
+              border: Border(
+                top: BorderSide(width: 1.0, color: Color(0xFFFF000000)),
+                left: BorderSide(width: 1.0, color: Color(0xFFFF000000)),
+                right: BorderSide(width: 1.0, color: Color(0xFFFF000000)),
+                bottom: BorderSide(width: 1.0, color: Color(0xFFFF000000)),
+              ),
+            ),*/
             child: Column(
               crossAxisAlignment:
                   CrossAxisAlignment.start, //met dans le bon axe
               children: [
                 Container(
-                  padding: const EdgeInsets.only(bottom: 10),
+                  padding: const EdgeInsets.fromLTRB(15, 20, 0, 0),
+                  height: 40,
+                  child: Text(
+                    'Informations\n',
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(50, 15, 0, 0),
                   child: Text(
                     'Kelly Kelly NightClub',
                     style: TextStyle(
@@ -53,7 +41,7 @@ class NightClubProfile extends StatelessWidget {
                   ),
                 ),
                 Container(
-                  padding: const EdgeInsets.only(bottom: 5),
+                  padding: const EdgeInsets.fromLTRB(50, 25, 0, 0),
                   child: Row(
                     crossAxisAlignment:
                         CrossAxisAlignment.start, //met dans le bonne axe
@@ -72,7 +60,7 @@ class NightClubProfile extends StatelessWidget {
                   ),
                 ),
                 Container(
-                  padding: const EdgeInsets.only(top: 5),
+                  padding: const EdgeInsets.fromLTRB(50, 25, 0, 0),
                   child: Row(
                     crossAxisAlignment:
                         CrossAxisAlignment.start, //met dans le bonne axe
@@ -92,7 +80,7 @@ class NightClubProfile extends StatelessWidget {
                   ),
                 ),
                 Container(
-                  padding: const EdgeInsets.only(top: 10),
+                  padding: const EdgeInsets.fromLTRB(50, 25, 0, 0),
                   child: Row(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
@@ -103,6 +91,139 @@ class NightClubProfile extends StatelessWidget {
                       ),
                       Text(
                         '   www.kellyKelly.com',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontStyle: FontStyle.italic,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+
+
+    Widget musicSection = Expanded(
+      child: Row(
+        children: [
+          Container(
+            margin: EdgeInsets.fromLTRB(50, 15, 0,0),
+            //width: 275,
+            //height: 150,
+            /*decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              //color: Colors.blueGrey,
+              border: Border(
+                top: BorderSide(width: 1.0, color: Color(0xFFFF000000)),
+                left: BorderSide(width: 1.0, color: Color(0xFFFF000000)),
+                right: BorderSide(width: 1.0, color: Color(0xFFFF000000)),
+                bottom: BorderSide(width: 1.0, color: Color(0xFFFF000000)),
+              ),
+            ),*/
+            child: Column(
+              crossAxisAlignment:
+              CrossAxisAlignment.start, //met dans le bon axe
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(15),
+                  height: 40,
+                  child: Text(
+                    'Musique\n',
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(50, 25, 0, 0),
+                  child: Row(
+                    crossAxisAlignment:
+                    CrossAxisAlignment.start, //met dans le bonne axe
+                    children: [
+                      Icon(
+                        Icons.music_note,
+                        color: Colors.amber,
+                        size: 17,
+                      ),
+                      Text(
+                        '   Style de Musique : Electro',
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(50, 25, 0, 0),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Icon(
+                        Icons.queue_music,
+                        size: 17,
+                        color: Colors.blue,
+                      ),
+                      Text(
+                        '   www.soundcloud.com',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontStyle: FontStyle.italic,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+
+    Widget upperSection = Container(
+      padding: const EdgeInsets.all(20), //offset de 50
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment:
+                  CrossAxisAlignment.start, //met dans le bon axe
+              children: [
+
+                Container(
+                  padding: const EdgeInsets.only(top: 5),
+                  child: Row(
+                    crossAxisAlignment:
+                        CrossAxisAlignment.start, //met dans le bonne axe
+                    children: [
+                      Icon(
+                        Icons.music_note,
+                        color: Colors.amber,
+                        size: 17,
+                      ),
+                      Text(
+                        '   Style de Musique : Electro',
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Icon(
+                        Icons.queue_music,
+                        size: 17,
+                        color: Colors.blue,
+                      ),
+                      Text(
+                        '   www.soundcloud.com',
                         style: TextStyle(
                           color: Colors.blue,
                           fontStyle: FontStyle.italic,
@@ -190,15 +311,12 @@ class NightClubProfile extends StatelessWidget {
         },
         body: Column(
           children: <Widget>[
-            upperSection,
-            textSection,
+            infoSection,
+            musicSection,
             priceSection,
           ],
         ),
-
-
       ),
-
     );
     /*return Scaffold(
       body: Column(
