1095be7524473b80a633f7771b089521963b1b24
BastinMax <maxed.bastin@gmail.com>
2019-05-14 14:49:52 +0200

    My reservation in progress, just a random list for now

---
+++ lib/myReservations/myReservation.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class ListReservation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final title = 'Mes reservations';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: ListView(
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.map),
+              title: Text('Map'),
+            ),
+            ListTile(
+              leading: Icon(Icons.photo_album),
+              title: Text('Album'),
+            ),
+            ListTile(
+              leading: Icon(Icons.phone),
+              title: Text('Phone'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
