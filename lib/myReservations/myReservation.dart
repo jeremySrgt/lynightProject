6f9f90a1756973d5f25e47450b47bc633c2ee0d3
BastinMax <maxed.bastin@gmail.com>
2019-05-15 14:34:36 +0200

    Update of My reservation Page without link

--- lib/myReservations/myReservation.dart
+++ lib/myReservations/myReservation.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:lynight/widgets/slider.dart';
+import 'package:lynight/myReservations/reservation.dart';
 
 class ListReservation extends StatelessWidget {
-  @override
+  //@override
   // Widget build(BuildContext context) {
   // final title = 'Mes reservations';
   //final myReservationList = 'Kelly Kelly';
@@ -9,9 +11,9 @@ class ListReservation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Mes reservation',
       color: Theme.of(context).primaryColor,
-      home: ListPage(title: 'Lessons'),
+      home: ListPage(title: 'Mes reservations'),
     );
   }
 }
@@ -26,68 +28,113 @@ class ListPage extends StatefulWidget {
 }
 
 class _ListPageState extends State<ListPage> {
+  List reservations;
+
+  @override
+  void initState() {
+    reservations = getReservations();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     final topAppBar = AppBar(
       elevation: 0.1,
       backgroundColor: Theme.of(context).primaryColor,
       title: Text(widget.title),
-
     );
 
-    final makeListTitle = ListTile(
-      contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-      leading: Container(
-        padding: EdgeInsets.only(right: 12.0),
-        decoration: BoxDecoration(
-            border: Border(
-          right: BorderSide(width: 1.0, color: Theme.of(context).primaryColor),
-        )),
-        child: Icon(Icons.music_note, color: Theme.of(context).primaryColor),
-      ),
-      title: Text(
-        'Test Mes reservations',
-        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-      ),
-      subtitle: Row(
-        children: <Widget>[
-          Icon(Icons.date_range, color: Theme.of(context).primaryColor),
-          Text('18/03', style: TextStyle(color: Colors.white))
-        ],
-      ),
-      trailing: Icon(Icons.keyboard_arrow_right, color: Colors.red, size: 30.0),
-    );
+    ListTile makeListTitle(Reservation reservation) => ListTile(
+          contentPadding:
+              EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+          leading: Container(
+            padding: EdgeInsets.only(right: 12.0),
+            decoration: BoxDecoration(
+                border: Border(
+              right:
+                  BorderSide(width: 1.0, color: Theme.of(context).primaryColor),
+            )),
+            child:
+                Icon(Icons.music_note, color: Theme.of(context).primaryColor),
+          ),
+          title: Text(
+            'Test Mes reservations',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          subtitle: Row(
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: Container(
+                  child: Icon(Icons.date_range,
+                      color: Theme.of(context).primaryColor),
+                ),
+              ),
+              Expanded(
+                flex: 4,
+                child: Padding(
+                  padding: EdgeInsets.only(left: 10.0),
+                  child: Text('18/03', style: TextStyle(color: Colors.white)) ,
+                ),
+              )
+            ],
+          ),
+          trailing:
+              Icon(Icons.keyboard_arrow_right, color: Colors.red, size: 30.0),
+      /*onTap: (){
+           context, MaterialPageRoute(builder: (context) => MyreservationPage()));
+      },*/
+        );
 
-
-
-    final makeCard = Card(
-      elevation: 8.0,
-      margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
-      child: Container(
-        decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
-        child: makeListTitle,
-      ),
-    );
-
+    Card makeCard(Reservation reservation) => Card(
+          elevation: 8.0,
+          margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+          child: Container(
+            decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
+            child: makeListTitle(reservation),
+          ),
+        );
 
     final makeBody = Container(
       child: ListView.builder(
         scrollDirection: Axis.vertical,
         shrinkWrap: true,
-        itemCount: 10,
+        itemCount: reservations.length,
         itemBuilder: (BuildContext context, int index) {
-          return makeCard;
+          return makeCard(reservations[index]);
         },
       ),
     );
-
-
 
     return Scaffold(
       //backgroundColor: Color.fromRGBO(58, 66, 86, 1.0),//gris
       appBar: topAppBar,
       body: makeBody,
+      /*drawer: CustomSlider(
+        userMail: 'Lalal',
+        signOut: ,
+        nameFirstPage: 'Profil',
+        routeFirstPage: '/userProfil',
+        nameSecondPage: 'Accueil',
+        routeSecondPage: '/',
+        nameThirdPage: 'Carte',
+        routeThirdPage: '/maps',
+      ),*/
     );
+  }
+
+  List getReservations() {
+    return [
+      Reservation(title: 'Kelly Kelly', date: '18/02'),
+      Reservation(
+        title: 'Jeremy\'s night club',
+        date: '03/04',
+      ),
+      Reservation(
+        title: 'Sysy\' club',
+        date: '15/05',
+      ),
+    ];
   }
 }
 /*
