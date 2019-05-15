900e24908da00e8867919b89f19cf5707e274199
BastinMax <maxed.bastin@gmail.com>
2019-05-15 15:10:58 +0200

    add New file needed for Reservation Page

---
+++ lib/myReservations/detailReservation.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:lynight/myReservations/myReservation.dart';
+import 'package:lynight/myReservations/reservation.dart';
+
+
+class DetailPage extends StatelessWidget{
+  final Reservation reservation;
+
+  DetailPage({Key key, this.reservation}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    final topContentText = Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        SizedBox(height: 120.0),
+        Icon(
+          Icons.play_arrow,
+          color: Colors.white,
+          size: 40.0,
+        ),
+        Container(
+          width: 90.0,
+          child: new Divider(color: Colors.green),
+        ),
+        SizedBox(height: 10.0),
+        Text(
+          reservation.title,
+          style: TextStyle(color: Colors.white, fontSize: 45.0),
+        ),
+        SizedBox(height: 30.0),
+
+      ],
+    );
+
+    final topContent = Stack(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.only(left: 10.0),
+          height: MediaQuery.of(context).size.height * 0.5,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("boite.jpg"),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        Container(
+          height: MediaQuery.of(context).size.height * 0.5,
+          padding: EdgeInsets.all(40.0),
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(color: Color.fromRGBO(58, 66, 86, .9)),
+          child: Center(
+            child: topContentText,
+          ),
+        ),
+        Positioned(
+          left: 8.0,
+          top: 60.0,
+          child: InkWell(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Icon(Icons.arrow_back, color: Colors.white),
+          ),
+        )
+      ],
+
+    );
+
+    final readButton = Padding(
+        padding: EdgeInsets.symmetric(vertical: 16.0),
+        child: RaisedButton(
+          onPressed: () => {},
+          color: Color.fromRGBO(58, 66, 86, 1.0),
+          child:
+          Text("QR Code maggl", style: TextStyle(color: Colors.white)),
+        ));
+
+    final bottomContentText = Text(
+      'QR Code',
+      style: TextStyle(fontSize: 18.0),
+    );
+
+    final bottomContent = Container(
+      // height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      // color: Theme.of(context).primaryColor,
+      padding: EdgeInsets.all(40.0),
+      child: Center(
+        child: Column(
+          children: <Widget>[bottomContentText, readButton],
+        ),
+      ),
+    );
+
+
+
+
+    return Scaffold(
+      body: Column(
+        children: <Widget>[topContent, bottomContent],
+      ),
+    );
+  }
+
+}
+
---
+++ lib/myReservations/reservation.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Reservation {
+  String title;
+  String date;
+
+  Reservation({this.title, this.date});
+
+}
