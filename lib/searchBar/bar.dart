b319db9724e92897bed5489bc8a478b6ae35bab5
sylvainjiang <sylvainjiang1@gmail.com>
2019-05-16 15:38:50 +0200

    merge

--- lib/searchBar/bar.dart
+++ lib/searchBar/bar.dart
@@ -83,55 +83,17 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class FavoritesNightClub extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return _FavoritesNightClubState();
-  }
-}
-
-class _FavoritesNightClubState extends State<FavoritesNightClub> {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-
-    final Iterable<Widget> tiles = _SuggestionListState()._saved.map(
-      (Club club) {
-        return Container(
-          child: ListView.builder(
-            scrollDirection: Axis.vertical,
-            shrinkWrap: true,
-            itemCount: _SuggestionListState()._saved.length,
-            itemBuilder: (BuildContext context, int index) {
-              return _SuggestionListState()
-                  ._makeCard(_SuggestionListState().clubs[index]);
-            },
-          ),
-        );
-      },
-    );
-    final List<Widget> divided = ListTile.divideTiles(
-      context: context,
-      tiles: tiles,
-      color: Theme.of(context).primaryColor,
-    ).toList();
-
-    return new Scaffold(
-      body: ListView(
-        children: divided,
-      ),
-    );
-  }
-}
-
 class SuggestionList extends StatefulWidget {
-  final String inputSearch;
 
   SuggestionList({Key key, this.inputSearch}) : super(key: key);
 
+  final String inputSearch;
+
   @override
-  _SuggestionListState createState() => new _SuggestionListState();
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _SuggestionListState();
+  }
 }
 
 class _SuggestionListState extends State<SuggestionList> {
@@ -222,7 +184,6 @@ class _SuggestionListState extends State<SuggestionList> {
                 _saved.add(club);
               }
             });
-           // FavoritesNightClub();
           },
         ));
   }
