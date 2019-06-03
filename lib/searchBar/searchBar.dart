aab32e176097c4ebf30ad42003da1d466e282f06
Yann <yann-kelly.say@edu.esiee.fr>
2019-06-03 13:53:01 +0200

    commit pull

--- lib/searchBar/searchBar.dart
+++ lib/searchBar/searchBar.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:lynight/searchBar/searchService.dart';
-import 'package:lynight/services/crud.dart';
 import 'package:lynight/nightCubPage/nightClubProfile.dart';
 
 class SearchBar extends StatefulWidget {
