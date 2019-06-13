adbfea96e4295034194c32824105685a14672b66
sokdavid <david.sok@edu.esiee.fr>
2019-06-13 10:32:29 +0200

    modif user profile and sum up design

--- lib/profilUtilisateur/profilUtilisateur.dart
+++ lib/profilUtilisateur/profilUtilisateur.dart
@@ -184,7 +184,6 @@ class _UserProfilState extends State<UserProfil> {
                         Container(
                           padding: EdgeInsets.only(top: 16),
                           width: MediaQuery.of(context).size.width,
-                          
                           decoration: BoxDecoration(
                             gradient: LinearGradient(
                                 colors: [
