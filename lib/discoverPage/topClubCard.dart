bf1c79fa961f29e6251debe896a74e7ff5eb86d7
sylvainjiang <sylvainjiang1@gmail.com>
2019-06-14 15:49:06 +0200

    top card debug music and separate code

--- lib/discoverPage/topClubCard.dart
+++ lib/discoverPage/topClubCard.dart
@@ -28,7 +28,7 @@ class _TopClubCardState extends State<TopClubCard> {
   Stream club;
   Map<dynamic, dynamic> testMap;
   List<String> clubSelected;
-  List<Map<dynamic,dynamic>> queryClubList;
+  List<Map<dynamic, dynamic>> queryClubList;
 
   bool electro = false;
   bool rap = false;
@@ -36,8 +36,6 @@ class _TopClubCardState extends State<TopClubCard> {
   bool populaire = false;
   bool rock = false;
   bool trans = false;
-
-
 
   //AlgoMusicReference algoTest = new AlgoMusicReference();
 
@@ -50,12 +48,11 @@ class _TopClubCardState extends State<TopClubCard> {
 //      });
 //    });
 
-
     crudObj.getDataDocuments('club').then((querySnapshot) {
       print(querySnapshot.documents[3].documentID);
-      List<Map<dynamic,dynamic>> tempList = [];
-      for(int i =0; i<querySnapshot.documents.length; i++){
-        Map<dynamic,dynamic> tempMap = querySnapshot.documents[i].data;
+      List<Map<dynamic, dynamic>> tempList = [];
+      for (int i = 0; i < querySnapshot.documents.length; i++) {
+        Map<dynamic, dynamic> tempMap = querySnapshot.documents[i].data;
         tempMap['clubID'] = querySnapshot.documents[i].documentID;
         tempList.add(tempMap);
       }
@@ -63,190 +60,212 @@ class _TopClubCardState extends State<TopClubCard> {
       setState(() {
         queryClubList = tempList;
       });
-
     });
   }
 
+  Widget clubPicture(height, width, i) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 22.0, right: 22),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(11.0),
+        child: Image.network(
+          queryClubList[i]['pictures'][0],
+          fit: BoxFit.cover,
+          height: height / 6,
+          width: width / 1.3,
+        ),
+      ),
+    );
+  }
+
+  Widget clubName(font, i) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 32.0, right: 25),
+      child: Text(
+        queryClubList[i]['name'],
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: font * 20,
+          color: Colors.white,
+        ),
+        textAlign: TextAlign.left,
+        maxLines: 2,
+      ),
+    );
+  }
+
+  Widget clubLocation(width, font, i) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20.0, right: 20),
+      child: Row(
+          //crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Icon(
+              Icons.location_on,
+              color: Colors.blue,
+            ),
+            SizedBox(
+              width: width / 40,
+            ),
+            Expanded(
+              child: Text(
+                '${queryClubList[i]['arrond']}e arrondissement',
+                style: TextStyle(
+                    color: Colors.white,
+                    height: 1.2,
+                    fontWeight: FontWeight.bold,
+                    fontSize: font * 13),
+                textAlign: TextAlign.justify,
+              ),
+            ),
+          ]),
+    );
+  }
+
+  Widget clubMusic(musicMap, width, font) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20.0, right: 25),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+        Icon(
+          Icons.music_note,
+          color: Colors.blue,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            musicMap['electro'] == true
+                ? Text(
+              'Electro ',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: font * 13),
+              overflow: TextOverflow.visible,
+            )
+                : Container(),
+            musicMap['populaire'] == true
+                ? Text(
+              'Populaire ',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: font * 13),
+              overflow: TextOverflow.visible,
+            )
+                : Container(),
+            musicMap['rap'] == true
+                ? Text(
+              'Rap ',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: font * 13),
+              overflow: TextOverflow.visible,
+            )
+                : Container(),
+            musicMap['rnb'] == true
+                ? Text(
+              'RnB ',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: font * 13),
+              overflow: TextOverflow.visible,
+            )
+                : Container(),
+            musicMap['rock'] == true
+                ? Text(
+              'Rock ',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: font * 13),
+              overflow: TextOverflow.visible,
+            )
+                : Container(),
+            musicMap['trans'] == true
+                ? Text(
+              'Psytrans ',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: font * 13),
+              overflow: TextOverflow.visible,
+            )
+                : Container(),
+          ],
+        ),
+      ]),
+    );
+  }
+
+  Widget clubList(i) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    double font = MediaQuery.of(context).textScaleFactor;
+
+    Map<String, dynamic> clubDataMap;
+    Map<dynamic, dynamic> musicMap = queryClubList[i]['musics'];
+
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) =>
+                    NightClubProfile(documentID: queryClubList[i]['clubID'])));
+      },
+      child: Container(
+        margin: EdgeInsets.only(left: 20, bottom: 20, right: 20),
+        //alignment: Alignment.topCenter,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color.fromRGBO(212, 63, 141, 1),
+                  Color.fromRGBO(2, 80, 197, 1)
+                ]),
+            //color: Theme.of(context).primaryColor,
+            borderRadius: BorderRadius.all(Radius.circular(25))),
+        width: width / 1.7,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              clubPicture(height, width, i),
+              SizedBox(
+                height: height / 40,
+              ),
+              clubName(font, i),
+              SizedBox(
+                height: height / 40,
+              ),
+              clubLocation(width, font, i),
+              SizedBox(
+                height: height / 75,
+              ),
+              clubMusic(musicMap, width, font),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: clubList(),
+      child: ListView.builder(
+          itemCount: queryClubList.length,
+          //faire une verif que la list ne soit pas null pour ca utiliser le refresh peut etre ca pourrait etre sympa
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, i) {
+            return clubList(i);
+          }),
     );
-  }
-
-
-  Widget clubList() {
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
-    double font = MediaQuery.of(context).textScaleFactor;
-    return ListView.builder(
-        itemCount: queryClubList.length, //faire une verif que la list ne soit pas null pour ca utiliser le refresh peut etre ca pourrait etre sympa
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, i) {
-
-//          Map<String, dynamic> clubDataMap;
-//
-//          String currentClubId =
-//              snapshot.data.documents[i].documentID;
-//          clubDataMap = snapshot.data.documents[i].data;
-
-          Map<dynamic, dynamic> musicMap = queryClubList[i]['musics'];
-
-          return GestureDetector(
-            onTap: () {
-              Navigator.push(context,MaterialPageRoute(builder: (context) => NightClubProfile(documentID:queryClubList[i]['clubID'])));
-            },
-            child: Container(
-              margin: EdgeInsets.only(left: 20,bottom: 20 ,right: 20),
-              //alignment: Alignment.topCenter,
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                      colors: [
-                        Color.fromRGBO(212, 63, 141, 1),
-                        Color.fromRGBO(2, 80, 197, 1)
-                      ]),
-                  //color: Theme.of(context).primaryColor,
-                  borderRadius: BorderRadius.all(Radius.circular(25))),
-              width: width/1.7,
-              child: Padding(
-                padding: const EdgeInsets.only(top: 20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Padding(
-                      padding:
-                      const EdgeInsets.only(left: 22.0, right: 22),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(11.0),
-                        child: Image.network(
-                          queryClubList[i]['pictures'][0],
-                          fit: BoxFit.cover,
-                          height: height/6,
-                          width: width/1.3,
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: height/40,
-                    ),
-                    Padding(
-                      padding:
-                      const EdgeInsets.only(left: 32.0, right: 25),
-                      child: Text(
-                        queryClubList[i]['name'],
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: font*20,
-                          color: Colors.white,),
-                        textAlign: TextAlign.left,
-                        maxLines: 2,
-                      ),
-                    ),
-                    SizedBox(
-                      height: height/40,
-                    ),
-                    Padding(
-                      padding:
-                      const EdgeInsets.only(left: 20.0, right: 20),
-                      child: Row(
-                        //crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Icon(
-                              Icons.location_on,
-                              color: Colors.blue,
-                            ),
-                            SizedBox(
-                              width: width/40,
-                            ),
-                            Expanded(
-                              child: Text(
-                                '${queryClubList[i]['arrond']}e arrondissement',
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    height: 1.2,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: font*13
-                                ),
-                                textAlign: TextAlign.justify,
-                              ),
-                            ),
-                          ]
-                      ),
-                    ),
-                    SizedBox(
-                      height: height/75,
-                    ),
-                    Padding(
-                      padding:
-                      const EdgeInsets.only(left: 20.0, right: 25),
-                      child: Row(children: <Widget>[
-                        Icon(
-                          Icons.music_note,
-                          color: Colors.blue,
-                        ),
-                        SizedBox(
-                          width: width/40,
-                        ),
-                        musicMap['electro'] == true
-                            ? Text(
-                          'Electro ',
-                          style:
-                          TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: font*13),
-                          overflow: TextOverflow.ellipsis,
-                        )
-                            : Container(),
-                        musicMap['populaire'] == true
-                            ? Text(
-                          'Populaire ',
-                          style:
-                          TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: font*13),
-                          overflow: TextOverflow.ellipsis,
-                        )
-                            : Container(),
-                        musicMap['rap'] == true
-                            ? Text(
-                          'Rap ',
-                          style:
-                          TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: font*13),
-                          overflow: TextOverflow.ellipsis,
-                        )
-                            : Container(),
-                        musicMap['rnb'] == true
-                            ? Text(
-                          'RnB ',
-                          style:
-                          TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: font*13),
-                          overflow: TextOverflow.ellipsis,
-                        )
-                            : Container(),
-                        musicMap['rock'] == true
-                            ? Text(
-                          'Rock ',
-                          style:
-                          TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: font*13),
-                          overflow: TextOverflow.ellipsis,
-                        )
-                            : Container(),
-                        Flexible(
-                          child: musicMap['trans'] == true
-                              ? Text(
-                            'Psytrans ',
-                            style:
-                            TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: font*13),
-                            overflow: TextOverflow.ellipsis,
-                          )
-                              : Container(),
-                        ),
-                      ]),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          );
-        });
   }
 }
