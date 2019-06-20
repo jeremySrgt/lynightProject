62e2729fe1f54e04f678c643a22b6b42267efeee
sylvainjiang <sylvainjiang1@gmail.com>
2019-06-20 14:04:35 +0200

    A propos update

--- lib/sliderInformations/about.dart
+++ lib/sliderInformations/about.dart
@@ -69,7 +69,7 @@ class _AboutState extends State<About> {
         subtitle: Text(
             ' \nBloon est une application mobile qui répertorie les boîtes de nuit à Paris.'
                 + ' Des boîtes de nuit seront recommandées selon votre profil personnalisé,'
-                + ' de plus, Bloon vous permet de réserver une place d\'entrée '
+                + ' invitez vos amis aux évenements que vous auriez réservés.'
                 + '\nLe droit à l\'accès est réservé au boîte de nuit.'
         ,style: TextStyle(color: Colors.black,height: 1.2),textAlign: TextAlign.justify,),
       ),
