ff7e4043f0d63286829a1e690ffed88fd9075b82
BastinMax <maxed.bastin@gmail.com>
2019-11-15 11:00:30 +0100

    QR code working

--- lib/comptePro/scannerQrCode.dart
+++ lib/comptePro/scannerQrCode.dart
@@ -19,8 +19,6 @@ class MyApp extends StatelessWidget {
 }
 
 class ScannerQrCode extends StatefulWidget {
-
-
   ScannerQrCode({this.onSignOut});
 
   final VoidCallback onSignOut;
@@ -36,17 +34,14 @@ class ScannerQrCode extends StatefulWidget {
     }
   }
 
-
   @override
   _ScannerQrCode createState() => _ScannerQrCode();
 }
 
 class _ScannerQrCode extends State<ScannerQrCode> {
-
   String userMail = 'userMail';
   String userId = 'userId';
-
-
+  bool _default = false;
 
   _qrCallback(String code) {
     setState(() {
@@ -57,13 +52,13 @@ class _ScannerQrCode extends State<ScannerQrCode> {
 
   String _qrInfo = 'Scan un code batard';
   bool _camState = false;
+
   _scanCode() {
     setState(() {
       _camState = true;
+      _default = true;
     });
   }
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -73,23 +68,66 @@ class _ScannerQrCode extends State<ScannerQrCode> {
       ),
       body: _camState
           ? Center(
-        child: SizedBox(
-          width: 512,
-          height: 1024,
-          child: QrCamera(
-            onError: (context, error) => Text(
-              error.toString(),
-              style: TextStyle(color: Colors.red),
-            ),
-            qrCodeCallback: (code) {
-              _qrCallback(code);
-            },
-          ),
-        ),
-      )
+              child: SizedBox(
+                width: 512,
+                height: 1024,
+                child: QrCamera(
+                  onError: (context, error) => Text(
+                    error.toString(),
+                    style: TextStyle(color: Colors.red),
+                  ),
+                  qrCodeCallback: (code) {
+                    _qrCallback(code);
+                  },
+                ),
+              ),
+            )
           : Center(
-        child: Text(_qrInfo),
-      ),
+              child: Container(
+                height: 200,
+                width: 300,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                  borderRadius: BorderRadius.circular(25.0),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    _default
+                        ? Center(
+                            child: Container(
+                              child: Column(
+                                children: <Widget>[
+                                  Text('Succes ! ',
+                                      style: TextStyle(
+                                          fontSize: 20, color: Colors.green)),
+                                  SizedBox(height: 15),
+                                  Text('Bienvenue ',
+                                      style: TextStyle(fontSize: 20)),
+                                  SizedBox(height: 15),
+                                  Text(_qrInfo, style: TextStyle(fontSize: 25))
+                                ],
+                              ),
+                            ),
+                          )
+                        : Center(
+                            child: Container(
+                              child: Column(
+                                children: <Widget>[
+                                  Text("Scannez un qr code",
+                                    style: TextStyle(
+                                      fontSize: 20
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                  ],
+                ),
+                alignment: Alignment(0, 0),
+              ),
+            ),
       floatingActionButton: Visibility(
         visible: !_camState,
         child: FloatingActionButton(
@@ -98,7 +136,7 @@ class _ScannerQrCode extends State<ScannerQrCode> {
           child: Icon(Icons.scanner),
         ),
       ),
-        drawer: CustomSlider(
+      drawer: CustomSlider(
         userMail: userMail,
         signOut: widget._signOut,
         activePage: 'ScanQr',
@@ -106,9 +144,6 @@ class _ScannerQrCode extends State<ScannerQrCode> {
     );
   }
 }
-
-
-
 
 //import 'package:flutter/material.dart';
 //import 'package:flutter/services.dart';
