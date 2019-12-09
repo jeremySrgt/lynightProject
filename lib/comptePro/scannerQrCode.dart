6423cf29551d381573112ae6c509a178317bd4ed
BastinMax <maxed.bastin@gmail.com>
2019-12-09 16:03:09 +0100

    First new version of QR Code scanning with Firebase and verif if code already scanned

--- lib/comptePro/scannerQrCode.dart
+++ lib/comptePro/scannerQrCode.dart
@@ -32,6 +32,9 @@ class _ScannerQrCode extends State<ScannerQrCode> {
   bool _default = false;
   List<dynamic> _listePlace;
   String testQrCode = "cSWrs7G9GUXvH9q8jlqH";
+  bool _qrEquals = false;
+  bool _alreadyScan = false;
+  bool _isScanning = false;
 
   @override
   void initState() {
@@ -48,12 +51,90 @@ class _ScannerQrCode extends State<ScannerQrCode> {
       });
     });
   }
+  Map<dynamic, dynamic>_maptest = {};
 
   _qrCallback(String code) {
+
+    test(code);
+    print(_maptest);
     setState(() {
       _camState = false;
       _qrInfo = code;
     });
+    print(_maptest);
+    /*for (int i = 0; i < _listePlace.length; i++) {
+      if (_qrInfo == _listePlace[i].documentID &&
+          _listePlace[i]["alreadyScan"] == false) {
+        print("------------------");
+        print(_listePlace[i]["alreadyScan"]);
+        Firestore.instance
+            .collection("club")
+            .document("-LhKMefcBQ5wcJwluZxY")
+            .collection("placesReservees")
+            .document(_listePlace[i].documentID)
+            .setData({"alreadyScan": true}, merge: true);
+        setState(() {
+          _qrEquals = true;
+          _alreadyScan = false;
+          print("Qr equals passe a true");
+        });
+      }else{
+        setState(() {
+          _alreadyScan = true;
+        });
+      }
+    }*/
+  }
+
+  test(code){
+    setState(() {
+      _isScanning = true;
+    });
+    Firestore.instance
+        .collection('club')
+        .document('-LhKMefcBQ5wcJwluZxY')
+        .collection('placesReservees')
+        .getDocuments()
+        .then((value) {
+//      setState(() {
+//        _listePlace = value.documents;
+//      });
+      for (int i = 0; i < value.documents.length; i++) {
+        print(value.documents[i].documentID);
+        print(value.documents[i]["alreadyScan"]);
+        if (code == value.documents[i].documentID && value.documents[i]["alreadyScan"] == false) {
+          print("DANS LE 1ER IF");
+          Firestore.instance
+              .collection("club")
+              .document("-LhKMefcBQ5wcJwluZxY")
+              .collection("placesReservees")
+              .document(value.documents[i].documentID)
+              .setData({"alreadyScan": true}, merge: true);
+          setState(() {
+            _maptest = {"code": code, "alreadyScan": false};
+          });
+          break;
+
+        }else if(code == value.documents[i].documentID && value.documents[i]["alreadyScan"] == true){
+          print("DANS LE 2ER IF");
+          setState(() {
+            _maptest = {"code": code, "alreadyScan": true};
+          });
+
+          break;
+        }else{
+          print("DANS LE 13ER IF");
+          setState(() {
+            _maptest = {};
+          });
+        }
+      }
+      setState(() {
+        _isScanning = false;
+      });
+    });
+
+
   }
 
   String _qrInfo = 'Scan un code batard';
@@ -81,9 +162,42 @@ class _ScannerQrCode extends State<ScannerQrCode> {
     );
   }
 
-  Widget compareQrCode(clubData, context) {
+  Widget compareQrCode() {
+    return Center(
+        child: _qrEquals
+            ? Container(
+                child: Column(
+                  children: <Widget>[
+                    Text("C'est bien le même QR Code" + "\n" + _qrInfo + "\n")
+                  ],
+                ),
+              )
+            : Container(
+                child: Column(
+                  children: <Widget>[
+                    Text("Pas les mêmes " +
+                        "\n" +
+                        _qrInfo +
+                        "\n" +
+                        _listePlace[2].documentID)
+                  ],
+                ),
+              ));
+  }
 
-    return Container();
+  Widget checkPlace() {
+    if(_isScanning){
+      return CircularProgressIndicator();
+    }
+    print(_maptest);
+    if(_maptest["alreadyScan"] == true ){
+      return Text("C'est PAS  bon DEJA SCAN! ");
+    }else if(_maptest["alreadyScan"] == false){
+      return Text("C'est bon bienvenu!");
+
+    }else{
+      return Text("Pas de placeeeeeeeeeeee");
+    }
   }
 
   Widget errorInScan() {
@@ -137,17 +251,7 @@ class _ScannerQrCode extends State<ScannerQrCode> {
                         ? Center(
                             child: Container(
                               child: Column(
-                                children: <Widget>[
-                                  _qrInfo == _listePlace[2].documentID
-                                  ? Text(
-                                        "Ah blabla c'est  le meme "
-                                      )
-                                  : Text(
-                                      "Pas les mêmes " + "\n" +
-                                          _qrInfo  + "\n" +
-                                          _listePlace[2].documentID
-                                  )
-                                ],
+                                children: <Widget>[checkPlace()],
                               ),
                             ),
                           )
